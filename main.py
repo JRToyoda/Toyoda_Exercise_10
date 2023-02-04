7c15672d9c7512de19997f739c8ebf12baef7f44
JRToyoda <therubikators@gmail.com>
2023-02-04 17:09:40 +0800

    version 2

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
-class Question:
-    def __init__(self):
-        self._question = ""
-        self._answer = ""
+from file2 import Choices
 
-    def setQuestion(self, questionText):
-        self._question = questionText + "?\n----------------"
-
-    def setAnswer(self, questionAnswer):
-        self._answer = questionAnswer
-
-    def checkAnswer(self, userAnswer):
-        return userAnswer == self._answer
-
-    def display(self):
-        print(self._question)
+def askUser(prompt):
+    prompt.display()
+    userAnswer = input("Your answer: ")
+    print()
+    print(prompt.checkAnswer(userAnswer), "\n")
 
 
-class Choices(Question):
-    def __init__(self):
-        super().__init__()
-        self._choices = []
+def quiz():
+    first_question = Choices()
+    first_question.setQuestion("112 x 3")
+    first_question.addChoice("326", False)
+    first_question.addChoice("336", True)
+    first_question.addChoice("347", False)
+    first_question.addChoice("356", False)
 
-    def addChoice(self, choice, correctChoice):
-        self._choices.append(choice)
-        if correctChoice:
-            answerPosition = str(len(self._choices))
-            self.setAnswer(answerPosition)
+    second_question = Choices()
+    second_question.setQuestion("12 x 17")
+    second_question.addChoice("304", False)
+    second_question.addChoice("214", False)
+    second_question.addChoice("204", True)
+    second_question.addChoice("224", False)
 
-    def display(self):
-        super().display()
+    third_question = Choices()
+    third_question.setQuestion("55 x 14")
+    third_question.addChoice("870", False)
+    third_question.addChoice("760", False)
+    third_question.addChoice("770", True)
+    third_question.addChoice("670", False)
 
-        for i in range(len(self._choices)):
-            position = i + 1
-            print("%d: %s" % (position, self._choices[i]))
+    fourth_question = Choices()
+    fourth_question.setQuestion("39 x 12")
+    fourth_question.addChoice("468", True)
+    fourth_question.addChoice("478", False)
+    fourth_question.addChoice("498", False)
+    fourth_question.addChoice("508", False)
+
+    fifth_question = Choices()
+    fifth_question.setQuestion("11 x 38")
+    fifth_question.addChoice("418", True)
+    fifth_question.addChoice("528", False)
+    fifth_question.addChoice("518", False)
+    fifth_question.addChoice("508", False)
+
+    askUser(first_question)
+    askUser(second_question)
+    askUser(third_question)
+    askUser(fourth_question)
+    askUser(fifth_question)
+
+
+quiz()
